ac36f0e21ce533cb904dd5ca63c9a1d2adb36937
lat9 <cindy@vinosdefrutastropicales.com>
2026-05-23 15:28:04 -0400

    `zen_config`, adding 2nd parameter
    ... as changed in the base via this PR: https://github.com/zencart/zencart/pull/7742

--- includes/functions/extra_functions/zen_config.php
+++ includes/functions/extra_functions/zen_config.php
@@ -8,8 +8,8 @@ if (!function_exists('zen_config')) {
      * Function to retrieve a database constant, presumed to be in either
      * the configuration or product_type_layout tables.
      */
-    function zen_config(string $key): mixed
+    function zen_config(string $key, mixed $default = null): mixed
     {
-        return defined($key) ? constant($key) : null;
+        return defined($key) ? constant($key) : $default;
     }
 }
